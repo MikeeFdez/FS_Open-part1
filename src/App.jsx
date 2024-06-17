a7465d2114e8daab95027d94fb997b96c14cd690
Mike Fernandez <mike.fernandez@outlook.es>
2024-06-17 19:00:24 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react"
+
+const Header = ({course}) => {
+  // console.log(course)
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({part, exercises}) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises}/>
+      <Part part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part part={parts[2].name} exercises={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  // console.log(amount)
+  return (
+    <p>Number of exercises: {parts}</p>
+  )
+}
+
+
+
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    }
+    setClicks(newClicks)
+    console.log(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+    console.log(newClicks)
+  }
+
+
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} /> 
+      <button onClick={handleLeftClick}>Click left</button>
+      <button onClick={handleRightClick}>Click right</button>
+    </div>
+  )
+}
+
+export default App
